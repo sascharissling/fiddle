a2d2d2cd6dce297dae052ff250f6de7f161de79c
sascharissling <sascha.rissling@gmail.com>
2019-12-18 11:50:58 +0100

    Add theming

---
+++ client/src/utils/themes.js
@@ -0,0 +1,11 @@
+const defaultTheme = {
+  primary: '#EA4F87',
+  secondary: '#707070',
+  tertiary: '#f5f5f5',
+  background: '#000000',
+  lightBackground: '#272727',
+  themeGradient: 'linear-gradient(180deg, #EA4F87 0%, #F37A6A 60%)',
+  textGradient: '-webkit-linear-gradient(#ea4f87 0%, #f37a6a 60%)'
+};
+
+export default defaultTheme;
