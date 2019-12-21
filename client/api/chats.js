0f98967709839b8fb06e721744cc5c7b4632c240
sascharissling <sascha.rissling@gmail.com>
2019-12-21 09:45:45 +0100

    Add getAllChats function to client API

--- client/api/chats.js
+++ client/api/chats.js
@@ -1,3 +1,18 @@
+//Get all chats
+export function getAllChats(chatId) {
+  return fetch(`/api/chats`, {
+    medhod: 'GET'
+  })
+    .then(response => {
+      if (response.status != 200) {
+        throw new Error(response.statusText);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+//Get Chat by ID
 export function getChat(chatId) {
   return fetch(`/api/chats/${chatId}`, {
     medhod: 'GET'
