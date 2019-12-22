50cc59da77ff8039ae878082ff52eb1fc3ee4967
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-12-22 10:48:13 +0100

    Backend (#25)
    * Add theme to Themeprovider
    
    * Add connection to MongoDB server
    
    * Connect to MongoDB server
    
    * Get "chats" collection from MongoDB server
    
    * Initialize DB
    
    * Install mongoose for server connection
    
    * Connect to MongoDB via Mongoose+Express
    
    * Write  and test post request
    
    * Update mongoose chatSchema
    
    * Write middleware
    
    * Create "get one" and "delete one" chat
    
    * Create patch to send single pieces of information
    
    * Add getChat by ID on client side
    
    * Add getAllChats function to client API
    
    * Add addNewChat function to client API
    
    * Fix Typos
    
    * Create addNewMessage function in client API
    
    * Create "get one chat" path
    
    * Delete database.js because it is not needed
    
    * Refactor Schema
    
    * Add "get user1", "get user2" endpoints to API
    
    * Add message Schema
    
    * Add message-creation-date
    
    * Refactor "get chats of one user" to make it work by cascading search queries in MongoDB
    
    * Add ID to newly created message
    
    * Delete messageSchema and import it to chat.js as it is directly required in the chat schema
    
    * Make "input one chat message" work by help from stack overflow
    
    * Change request for get one chat and make it work without middleware

---
+++ client/api/chats.js
@@ -0,0 +1,49 @@
+//Get all chats
+export function getAllChats() {
+  return fetch('/api/chats', {
+    method: 'GET'
+  })
+    .then(response => {
+      if (response.status != 200) {
+        throw new Error(response.statusText);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+//Get Chat by ID
+export function getChat(chatId) {
+  return fetch(`/api/chats/${chatId}`, {
+    method: 'GET'
+  })
+    .then(response => {
+      if (response.status != 200) {
+        throw new Error(response.statusText);
+      }
+      return response;
+    })
+    .then(response => response.json());
+}
+
+//Add New Chat
+export function addNewChat(chat) {
+  return fetch('/api/chats', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(chat)
+  }).then(response => response.json());
+}
+
+//Add New Message
+export function addNewMessage(id, message) {
+  return fetch(`/api/chats/${id}/messages`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(message)
+  }).then(response => response.json());
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to DB'));
+
+const app = express();
+
+app.use(express.json());
+
+const chatsRouter = require('./lib/chats');
+app.use('/chats', chatsRouter);
+
+app.listen(8080, () => {
+  console.log('Server ready on http://localhost:8080');
+});
