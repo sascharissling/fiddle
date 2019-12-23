d8144ba2f21b34be38f4adc01e31a4f5c9abb844
sascharissling <sascha.rissling@gmail.com>
2019-12-23 11:12:20 +0100

    Import svg into component and style it right there

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -2,34 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styled from '@emotion/styled';
-import backIcon from '../assets/backIcon';
 
 //STYLE start
 
-const Button = styled.button`
+const ProxyButton = styled.button`
   background: transparent;
-
   margin: 0;
   padding: 0;
-
   display: flex;
   align-content: center;
   align-items: center;
   justify-content: center;
-
   cursor: pointer;
 `;
 
-const BackArrow = styled.img``;
+const BackArrow = styled.path`
+  fill: ${props => props.theme.primary};
+`;
 
 //STYLE end
 
 export default function BackButton({ location }) {
   return (
     <Link to={location}>
-      <Button>
-        <BackArrow src={backIcon} />
-      </Button>
+      <ProxyButton>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="8.537"
+          height="18.165"
+          viewBox="0 0 8.537 18.165"
+        >
+          <BackArrow
+            id="chevron-thin-left"
+            d="M16.177,19.156a.809.809,0,0,1,0,1.034.578.578,0,0,1-.9,0L8.013,11.838a.809.809,0,0,1,0-1.034l7.265-8.352a.579.579,0,0,1,.9,0,.809.809,0,0,1,0,1.034L9.551,11.321l6.626,7.834Z"
+            transform="translate(-7.826 -2.239)"
+          />
+        </svg>
+      </ProxyButton>
     </Link>
   );
 }
