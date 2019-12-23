938f8695cb82740d104d56b5b092d50ff7ef1c07
sascharissling <sascha.rissling@gmail.com>
2019-12-23 11:32:03 +0100

    Delete link for Storybook testing as well as Add onClick event

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import styled from '@emotion/styled';
 
 //STYLE start
 
 const ProxyButton = styled.button`
+  border: none;
   background: transparent;
   margin: 0;
   padding: 0;
@@ -22,23 +21,21 @@ const BackArrow = styled.path`
 
 //STYLE end
 
-export default function BackButton({ location }) {
+export default function BackButton({ onClick }) {
   return (
-    <Link to={location}>
-      <ProxyButton>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="8.537"
-          height="18.165"
-          viewBox="0 0 8.537 18.165"
-        >
-          <BackArrow
-            id="chevron-thin-left"
-            d="M16.177,19.156a.809.809,0,0,1,0,1.034.578.578,0,0,1-.9,0L8.013,11.838a.809.809,0,0,1,0-1.034l7.265-8.352a.579.579,0,0,1,.9,0,.809.809,0,0,1,0,1.034L9.551,11.321l6.626,7.834Z"
-            transform="translate(-7.826 -2.239)"
-          />
-        </svg>
-      </ProxyButton>
-    </Link>
+    <ProxyButton onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="8.537"
+        height="18.165"
+        viewBox="0 0 8.537 18.165"
+      >
+        <BackArrow
+          id="chevron-thin-left"
+          d="M16.177,19.156a.809.809,0,0,1,0,1.034.578.578,0,0,1-.9,0L8.013,11.838a.809.809,0,0,1,0-1.034l7.265-8.352a.579.579,0,0,1,.9,0,.809.809,0,0,1,0,1.034L9.551,11.321l6.626,7.834Z"
+          transform="translate(-7.826 -2.239)"
+        />
+      </svg>
+    </ProxyButton>
   );
 }
