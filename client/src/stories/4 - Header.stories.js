191b77cdade1e6459011231cc7195f0dfe1539d3
sascharissling <sascha.rissling@gmail.com>
2019-12-23 20:11:07 +0100

    Rename story files

---
+++ client/src/stories/4 - Header.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+//COMPONENTS Imports
+import HeaderBar from '../components/layout/HeaderBar';
+import BackButton from '../components/buttons/BackButton';
+import NewChatButton from '../components/buttons/NewChatButton';
+
+export default {
+  title: 'Navigation'
+};
+
+export function ChatsNavigation() {
+  return (
+    <HeaderBar>
+      <BackButton onClick={action('clicked')} />
+      <NewChatButton onClick={action('clicked')} />
+    </HeaderBar>
+  );
+}
---
+++ client/src/stories/5 - AudioInterface.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs } from '@storybook/addon-knobs';
+
+//COMPONENTS imports
+import FiddleButton from '../components/buttons/FiddleButton';
+import RecordButton from '../components/buttons/RecordButton';
+import PlayButton from '../components/buttons/PlayButton';
+import StopButton from '../components/buttons/StopButton';
+import PauseButton from '../components/buttons/PauseButton';
+
+export default {
+  title: 'Audio Actions',
+  decorators: [withKnobs]
+};
+
+//Button leads to where a new audio file can be recorded
+export function GoFiddle() {
+  return <FiddleButton onClick={action('clicked')} />;
+}
+
+export function Record() {
+  return <RecordButton onClick={action('clicked')} />;
+}
+
+export function Play() {
+  return <PlayButton onClick={action('clicked')} />;
+}
+
+export function Pause() {
+  return <PauseButton onClick={action('clicked')} />;
+}
+
+export function Stop() {
+  return <StopButton onClick={action('clicked')} />;
+}
