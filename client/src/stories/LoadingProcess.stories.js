9d960522e5002264fdb950320784cd69056b5261
sascharissling <sascha.rissling@gmail.com>
2019-12-23 14:18:27 +0100

    Test Loading Line successfully in storybook

---
+++ client/src/stories/LoadingProcess.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//COMPONENTS Imports
+import LoadingLine from '../components/LoadingLine';
+
+export default {
+  title: 'Loading Process'
+};
+
+export function Loading() {
+  return <LoadingLine />;
+}
