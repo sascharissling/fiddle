6989c3093a6585a396a42b6b2e4eaab9f16a4a61
sascharissling <sascha.rissling@gmail.com>
2019-12-23 11:09:04 +0100

    Create HeaderBar component which is used multiple times in this application

---
+++ client/src/components/HeaderBar.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const HeaderBar = styled.header`
+  width: 100vw;
+  height: 70px;
+  display: flex;
+  background: ${props => props.theme.background};
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  padding-left: 20px;
+`;
+
+export default HeaderBar;
