c4b8555802442639ce851cab983fb42bc0fad682
sascharissling <sascha.rissling@gmail.com>
2019-12-26 11:09:56 +0100

    Created user login using local storage for now

--- client/src/components/inputs/UserNameInput.js
+++ client/src/components/inputs/UserNameInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Redirect } from 'react-router-dom';
 
 //STYLE start
 
@@ -18,21 +19,33 @@ const NameInput = styled.input`
 
 //STYLE end
 
-export default function UserNameInput({ onSubmit }) {
+function upperCaseFirstChar(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export default function UserNameInput() {
   const [userName, setUserName] = React.useState('');
+  const [definedUsername, setDefinedUsername] = React.useState(false);
+
+  function handleChange(event) {
+    const upperCasedUserName = upperCaseFirstChar(event.target.value);
+    setUserName(upperCasedUserName);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    onSubmit(userName);
-    setUserName('');
+    localStorage.setItem('userName', `${userName}`);
+    setDefinedUsername(true);
   }
-  function handleChange(event) {
-    const value = event.target.value;
-    setUserName(value);
-  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <NameInput autoFocus type="text" value={userName} onChange={handleChange} />
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <NameInput autoFocus type="text" value={userName} onChange={handleChange} />
+      </form>
+      {definedUsername && (
+        <Redirect to={`/chatlist?userName=${localStorage.getItem('userName')}`} />
+      )}
+    </>
   );
 }
