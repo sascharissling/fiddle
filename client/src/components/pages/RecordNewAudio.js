838c805e5bbf6ed9a4f03d63c92a25926dccc881
sascharissling <sascha.rissling@gmail.com>
2020-01-07 13:54:22 +0100

    Refactor recording of new audio

--- client/src/components/pages/RecordNewAudio.js
+++ client/src/components/pages/RecordNewAudio.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link, Redirect } from 'react-router-dom';
-import { sendChatMessage, uploadAudio, deleteAudio } from '../../api/chats';
-import { ReactMic } from 'react-mic';
+import { sendChatMessage, uploadAudio } from '../../api/chats';
 import PropTypes from 'prop-types';
 
 //COMPONENTS imports
 import HeaderBar from '../layout/HeaderBar';
 import BackButton from '../buttons/BackButton';
-import NoAudioYet from '../headlines/NoAudioYet';
-import AudioInterfaceWrapper from '../audioInterface/AudioInterfaceWrapper';
-import RecordNewFiddle from '../audioInterface/RecordNewFiddle';
-import RecordButton from '../buttons/RecordButton';
-import StopButton from '../buttons/StopButton';
-import FiddleDisplay from '../audioInterface/FiddleDisplay';
-import LoadingLineLong from '../misc/LoadingLineLong';
-import SendAudio from '../buttons/SendAudio';
-import DiscardAudio from '../buttons/DiscardAudio';
+import PlayJustRecordedAudio from '../audioInterface/PlayJustRecordedAudio';
+import InitialAudioRecording from '../audioInterface/InitialAudioRecording';
 
 //STYLE start
 
@@ -26,63 +18,22 @@ const RecordPage = styled.div`
   display: flex;
   flex-flow: column nowrap;
 `;
-
-const Mic = styled(ReactMic)`
-  opacity: 0;
-  height: 0;
-  width: 0;
-`;
-
-const FileHandling = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-around;
-  align-items: center;
-  align-content: center;
-  margin-top: 30px;
-  width: 100vw;
-  div {
-    display: flex;
-    justify-content: center;
-    width: 50%;
-  }
-`;
 //STYLE end
 
 export default function RecordNewAudio(props) {
   const chatId = props.match.params.id;
-  const [noAudioYet, setNoAudioYet] = React.useState(true);
   const [recordingDone, setRecordingDone] = React.useState(false);
-  const [isRecording, setIsRecording] = React.useState(false);
-  const [isProcessing, setIsProcessing] = React.useState(false);
   const [redirectToChat, setRedirectToChat] = React.useState(false);
   const [audioFileUrl, setAudioFileUrl] = React.useState('');
-  const [recordingDate, setRecordingDate] = React.useState('');
 
   // Send Chatmessage
   const type = 'audio';
   const body = audioFileUrl;
   const author = localStorage.getItem('userName');
 
-  function startRecording() {
-    setNoAudioYet(false);
-    setIsRecording(true);
-  }
-
-  function handleData(recordedBlob) {
-    console.log('chunk of real-time data is: ', recordedBlob);
-  }
-
-  function stopRecording() {
-    setIsRecording(false);
-  }
-
   async function handleStop(recordedBlob) {
-    setIsProcessing(true);
-    // Transform blob to base64 format so it can be uploaded to cloudinary with a unique file name
     const file_reader = new FileReader();
     const dateOfRecording = Date.now();
-    setRecordingDate(dateOfRecording);
     file_reader.readAsDataURL(recordedBlob.blob);
     file_reader.onloadend = async function() {
       const base64_string = file_reader.result;
@@ -94,7 +45,6 @@ export default function RecordNewAudio(props) {
     );
     setTimeout(() => {
       setRecordingDone(true);
-      setIsProcessing(false);
     }, 1500);
   }
 
@@ -102,8 +52,8 @@ export default function RecordNewAudio(props) {
     sendChatMessage(body, author, type, chatId);
     setRedirectToChat(true);
   }
+
   function handleDelete() {
-    deleteAudio(author, chatId, recordingDate);
     setRedirectToChat(true);
   }
 
@@ -114,46 +64,14 @@ export default function RecordNewAudio(props) {
           <BackButton />
         </Link>
       </HeaderBar>
-      {!recordingDone && (
-        <AudioInterfaceWrapper>
-          {noAudioYet && (
-            <>
-              <NoAudioYet />
-            </>
-          )}
-          {!noAudioYet && (
-            <>
-              <Mic
-                record={isRecording}
-                onStop={handleStop}
-                onData={handleData}
-                strokeColor="#000000"
-                backgroundColor="white"
-                mimeType="audio/webm"
-              />
-              {isProcessing && <LoadingLineLong />}
-              {!isProcessing && <RecordNewFiddle />}
-            </>
-          )}
-          {isRecording && <StopButton onClick={stopRecording} />}
-          {!isRecording && !isProcessing && <RecordButton onClick={startRecording} />}
-        </AudioInterfaceWrapper>
-      )}
+      {!recordingDone && <InitialAudioRecording handleStop={handleStop} />}
 
       {recordingDone && (
-        <>
-          <AudioInterfaceWrapper>
-            <FiddleDisplay audioFileUrl={audioFileUrl} />
-            <FileHandling>
-              <div>
-                <DiscardAudio onClick={handleDelete} />
-              </div>
-              <div>
-                <SendAudio onClick={handleSend} />
-              </div>
-            </FileHandling>
-          </AudioInterfaceWrapper>
-        </>
+        <PlayJustRecordedAudio
+          audioFileUrl={audioFileUrl}
+          handleDelete={handleDelete}
+          handleSend={handleSend}
+        />
       )}
       {redirectToChat && <Redirect to={`/chat/${chatId}`} />}
     </RecordPage>
