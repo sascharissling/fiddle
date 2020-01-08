12e734fbc1d8356d744a342af8bee67ad9d2267e
sascharissling <sascha.rissling@gmail.com>
2020-01-08 12:40:34 +0100

    Create page for audio overdub

---
+++ client/src/components/pages/OverdubAudio.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+//COMPONENTS imports
+import HeaderBar from '../layout/HeaderBar';
+import BackButton from '../buttons/BackButton';
+import AudioInterfaceWrapper from '../audioInterface/AudioInterfaceWrapper';
+import OverdubFiddle from '../audioInterface/OverdubFiddle';
+
+//STYLE start
+
+const OverdubAudioPage = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+//STYLE end
+
+export default function OverdubAudio(props) {
+  const chatId = props.match.params.id;
+  const fileName = props.match.params.originalFileName;
+  const audioFileUrl = `https://res.cloudinary.com/fiddle/video/upload/${fileName}`;
+
+  return (
+    <OverdubAudioPage>
+      <HeaderBar>
+        <Link to={`/chat/${chatId}`}>
+          <BackButton />
+        </Link>
+      </HeaderBar>
+      <AudioInterfaceWrapper>
+        <OverdubFiddle originalAudioFileUrl={audioFileUrl} chatId={chatId} />
+      </AudioInterfaceWrapper>
+    </OverdubAudioPage>
+  );
+}
+
+OverdubAudio.propTypes = {
+  match: PropTypes.string
+};
