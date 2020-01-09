8a3abed58cf20be4117ebdb4f4d1efb5b7bbf120
sascharissling <sascha.rissling@gmail.com>
2020-01-09 16:42:58 +0100

    Change connect route back

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGO_DB_URL}${process.env.MONGO_DB_NAME}`, {
+mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
