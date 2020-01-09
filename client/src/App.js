bd78fd888df81f5207ebd359d83062eba1fbd8e3
sascharissling <sascha.rissling@gmail.com>
2020-01-09 18:03:45 +0100

    Implement loading screen into chatlist

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //PAGES imports
 import WelcomeScreen from './components/pages/WelcomeScreen';
 import UserLogin from './components/pages/UserLogin';
-import LoadingScreen from './components/pages/LoadingScreen';
 import ChatList from './components/pages/ChatList';
 import Chat from './components/pages/Chat';
 import NewChat from './components/pages/NewChat';
@@ -12,13 +11,13 @@ import RecordNewAudio from './components/pages/RecordNewAudio';
 import PlayAudio from './components/pages/PlayAudio';
 import OverdubAudio from './components/pages/OverdubAudio';
 import PlayConsolidatedAudio from './components/pages/PlayConsolidatedAudio';
+import Consolidation from './components/pages/Consolidation';
 
 //STYLE imports
 import styled from '@emotion/styled';
 import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'emotion-theming';
 import defaultTheme from './utils/defaultTheme';
-import Consolidation from './components/pages/Consolidation';
 
 //STYLE start
 
@@ -38,7 +37,6 @@ function App() {
           <Switch>
             <Route path="/" exact component={WelcomeScreen} />
             <Route path="/login" component={UserLogin} />
-            <Route path="/loading" component={LoadingScreen} />
             <Route path="/chatlist" component={ChatList} />
             <Route path="/chat/:id" component={Chat} />
             <Route path="/newChat" component={NewChat} />
--- client/src/components/pages/ChatList.js
+++ client/src/components/pages/ChatList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import useGetUserChats from '../hooks/useGetUserChats';
 
 //COMPONENTS imports
@@ -10,6 +10,9 @@ import HeaderBar from '../layout/HeaderBar';
 import PageHeadline from '../headlines/PageHeadline';
 import HeadlineBar from '../layout/HeadlineBar';
 import NewChatButton from '../buttons/NewChatButton';
+import WelcomeUser from '../headlines/WelcomeUser';
+import FiddleSmallLogo from '../branding/FiddleSmallLogo';
+import { LoadingLine } from '../misc/LoadingLine';
 
 //SVG imports
 import DefaultUser0 from '../icons/DefaultUser0.svg';
@@ -17,6 +20,17 @@ import DefaultUser1 from '../icons/DefaultUser1.svg';
 import DefaultUser2 from '../icons/DefaultUser2.svg';
 
 //STYLE start
+
+const LoadingPage = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
+
 const ChatListPage = styled.main`
   height: 100vh;
   width: 100vw;
@@ -37,10 +51,17 @@ const ChatLink = styled(Link)`
 //STYLE end
 
 export default function ChatList() {
+  const [loadingDone, setLoadingDone] = React.useState(false);
   const userName = localStorage.getItem('userName');
   const userChats = useGetUserChats(userName);
   const reversedChats = userChats.reverse();
   const avatars = [DefaultUser0, DefaultUser1, DefaultUser2];
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoadingDone(true);
+    }, 1450);
+  }, []);
 
   function pickPartnerName(userName, user1, user2) {
     const partner = userName === user1 ? user2 : user1;
@@ -48,30 +69,45 @@ export default function ChatList() {
   }
 
   return (
-    <ChatListPage>
-      <HeaderBar>
-        <Link to={'/login'}>
-          <BackButton />
-        </Link>
-        <Link to={`/newChat/?userName=${localStorage.getItem('userName')}`}>
-          <NewChatButton />
-        </Link>
-      </HeaderBar>
-      <HeadlineBar>
-        <PageHeadline headline={'Chats'} />
-      </HeadlineBar>
-      <Chats>
-        {reversedChats.map(chat => (
-          <ChatLink key={chat._id} to={`/chat/${chat._id}`}>
-            <ChatListItem
-              partnerName={pickPartnerName(userName, chat.user1, chat.user2)}
-              userImgSrc={avatars[Math.floor(Math.random() * avatars.length)]}
-              lastMessage={chat.messages[chat.messages.length - 1].body}
-              lastMessageDate={chat.messages[chat.messages.length - 1].date.slice(11, 16)}
-            />
-          </ChatLink>
-        ))}
-      </Chats>
-    </ChatListPage>
+    <>
+      {!loadingDone && (
+        <LoadingPage>
+          <WelcomeUser userName={`${localStorage.getItem('userName')}`} />
+          <LoadingLine />
+          {loadingDone && (
+            <Redirect to={`/chatlist?userName=${localStorage.getItem('userName')}`} />
+          )}
+          <FiddleSmallLogo />
+        </LoadingPage>
+      )}
+
+      {loadingDone && (
+        <ChatListPage>
+          <HeaderBar>
+            <Link to={'/login'}>
+              <BackButton />
+            </Link>
+            <Link to={`/newChat/?userName=${localStorage.getItem('userName')}`}>
+              <NewChatButton />
+            </Link>
+          </HeaderBar>
+          <HeadlineBar>
+            <PageHeadline headline={'Chats'} />
+          </HeadlineBar>
+          <Chats>
+            {reversedChats.map(chat => (
+              <ChatLink key={chat._id} to={`/chat/${chat._id}`}>
+                <ChatListItem
+                  partnerName={pickPartnerName(userName, chat.user1, chat.user2)}
+                  userImgSrc={avatars[Math.floor(Math.random() * avatars.length)]}
+                  lastMessage={chat.messages[chat.messages.length - 1].body}
+                  lastMessageDate={chat.messages[chat.messages.length - 1].date.slice(11, 16)}
+                />
+              </ChatLink>
+            ))}
+          </Chats>
+        </ChatListPage>
+      )}
+    </>
   );
 }
