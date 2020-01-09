2024a9e91584aa9e473e3b1b242b18a0965d289d
sascharissling <sascha.rissling@gmail.com>
2020-01-09 16:45:34 +0100

    Prevent deprecation warnings in connection directly

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
