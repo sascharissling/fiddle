61de6cb199726ca3b0f30cdfe0b441e353fda2b1
sascharissling <sascha.rissling@gmail.com>
2020-01-09 16:39:00 +0100

    Rename db connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
+mongoose.connect(`${process.env.MONGO_DB_URL}${process.env.MONGO_DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
