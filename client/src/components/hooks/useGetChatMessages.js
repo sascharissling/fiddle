b79f4ff4df90d59fee041df59ea4df2c37387fbc
sascharissling <sascha.rissling@gmail.com>
2020-01-11 00:55:47 +0100

    Fetch chat messages via websocket

--- client/src/components/hooks/useGetChatMessages.js
+++ client/src/components/hooks/useGetChatMessages.js
@@ -5,13 +5,28 @@ export default function useGetChatMessages(_id) {
   const [chatMessages, setChatMessages] = React.useState([]);
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      getChatMessages(_id).then(fetchedChatMessages => {
-        setChatMessages(fetchedChatMessages);
-      });
-    }, 2000);
-    return () => clearInterval(interval);
+    const io = require('socket.io-client');
+    const socket = io('http://localhost:9090');
+    socket.emit('send-chat-id', _id);
+    socket.on('chat-messages', data => {
+      setChatMessages(data.messages);
+    });
   }, [_id]);
 
   return chatMessages;
 }
+
+// export default function useGetChatMessages(_id) {
+//   const [chatMessages, setChatMessages] = React.useState([]);
+
+//   React.useEffect(() => {
+//     const interval = setInterval(() => {
+//       getChatMessages(_id).then(fetchedChatMessages => {
+//         setChatMessages(fetchedChatMessages);
+//       });
+//     }, 2000);
+//     return () => clearInterval(interval);
+//   }, [_id]);
+
+//   return chatMessages;
+// }
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const io = require('socket.io')(9090, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
+      'Access-Control-Allow-Credentials': true
+    };
+    res.writeHead(200, headers);
+    res.end();
+  }
+});
 
 mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
   useNewUrlParser: true,
@@ -21,4 +32,20 @@ app.use('/api', chatsRouter);
 
 app.listen(8080, () => {
   console.log('Server ready on http://localhost:8080');
+});
+
+//SOCKET.io for getting messages
+io.on('connection', socket => {
+  console.log('user arrived');
+  socket.on('send-chat-id', _id => {
+    const chats = db.collection('chats');
+    const ObjectId = require('mongodb').ObjectID;
+    const chatId = new ObjectId(_id);
+    chats.findOne(chatId, function(error, result) {
+      if (error) {
+        throw error;
+      }
+      socket.emit('chat-messages', result);
+    });
+  });
 });
