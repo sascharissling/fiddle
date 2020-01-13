a6b72a0269e6a088316bc5de69bba06600c40b6a
sascharissling <sascha.rissling@gmail.com>
2020-01-13 11:26:44 +0100

    Change port variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const PORT = process.env.LOCAL_PORT || 8080;
 
 mongoose.connect(`${process.env.MONGO_DB_URL}${process.env.MONGO_DB_NAME}`, {
   useNewUrlParser: true,
@@ -28,6 +29,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(process.env.LOCAL_PORT, () => {
+app.listen(PORT, () => {
   console.log('Server ready on http://localhost:8080');
 });
