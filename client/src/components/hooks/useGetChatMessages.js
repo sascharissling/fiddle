bb76dba2ba7120b11a5856e0544a83a29b160758
sascharissling <sascha.rissling@gmail.com>
2020-01-13 15:30:00 +0100

    Add incoming sound for new messages

--- client/src/components/hooks/useGetChatMessages.js
+++ client/src/components/hooks/useGetChatMessages.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import UIfx from 'uifx';
+import incoming from '../../assets/incoming_message.mp3';
+
+const notification = new UIfx(incoming, {
+  volume: 0.3,
+  throttleMs: 100
+});
 
 export default function useGetChatMessages(_id) {
   const [chatMessages, setChatMessages] = React.useState([]);
@@ -12,6 +19,7 @@ export default function useGetChatMessages(_id) {
     });
     socket.on('new-chat-message', message => {
       setChatMessages(messages => [...messages, message]);
+      notification.play();
     });
   }, [_id]);
 
