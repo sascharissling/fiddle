c6d99aa6501be0b3a6a57b66251c7a84b672354a
sascharissling <sascha.rissling@gmail.com>
2020-01-13 11:19:24 +0100

    Add heroku configuration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/fiddle`, {
+mongoose.connect(`${process.env.MONGO_DB_URL}${process.env.MONGO_DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
